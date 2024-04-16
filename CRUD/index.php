f8701836efc13acf46ce1e5bf448165c5318d208
danmaciel23 <danilo.maciel11@gmail.com>
2024-04-16 16:09:49 -0300

    Quarta feature - Home com visualizacao de arquivos

---
+++ CRUD/index.php
@@ -0,0 +1,42 @@
+<?php 
+  
+    include('conexao.php');
+?>
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="x-ua-compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--    BootStrap-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <title>Dados dos alunos</title>
+
+</head>
+    <body style="text-align: center;">
+        <header class="page-header">
+            <div class="page-header">
+                <h1 class="display-1 text-center" style="background-color: lightblue"><a href="adminHome.php" style="text-decoration: none; color: black; font-size: 50pt">TechFit Academy</a></h1>
+            </div>
+        </header>      
+        <h1>Visualizar informações</h1>
+
+            <a href="ficha.php">
+            <button type="button" class="btn btn-primary">Visualizar Ficha</button>
+            </a>
+            
+            <a href="avaliacao.php">                
+                <button type="avaliacao.php" class="btn btn-primary">Visualizar Avaliação</button>                   
+            </a>            
+
+       
+    
+    </body>
+
+<header>
+
+    <menu class="menu top-menu">
+
+    </menu>
+</header>
