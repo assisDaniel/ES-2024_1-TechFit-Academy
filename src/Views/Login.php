0f50b1701dd6b0fd7f9a3fa89ec03de06f491ed4
danmaciel23 <danilo.maciel11@gmail.com>
2024-06-04 17:33:58 -0300

    Correcao de escala

--- src/Views/Login.php
+++ src/Views/Login.php
@@ -6,34 +6,37 @@
     <title>login</title>
     <link rel="stylesheet" href="/src/Views/static/css/LoginStyle.css">
     <link rel="icon" href="/src/Views/static/icons/favicon.svg" type="image/svg">
+    <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@1,900&family=Poppins:wght@400;600&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
 </head>
 <body>
-<div class="img-style"><img src="/src/Views/static/icons/logo-techfit1.svg" alt="logo"></div>
-<h1>Login</h1>
-<div class="form">
-    <?php
-    if (isset($_SESSION['camposInvalidos'])) {
-        echo '<p>' . $_SESSION['camposInvalidos'] . '</p>';
-        unset($_SESSION['camposInvalidos']);
-    }
-
-    if (isset($_SESSION['erros'])) {
-        foreach ($_SESSION['erros'] as $campo => $erro) {
-            echo '<p>' . $campo . ':' . $erro . '</p>';
+<div class="tudo">    
+    <div class="img-style"><img src="/src/Views/static/icons/logo-techfit1.svg" alt="logo"></div>
+    <h1>Login</h1>
+    <div class="form">
+        <?php
+        if (isset($_SESSION['camposInvalidos'])) {
+            echo '<p>' . $_SESSION['camposInvalidos'] . '</p>';
+            unset($_SESSION['camposInvalidos']);
         }
-        unset($_SESSION['erros']);
-    }
-    ?>
-    <form action="login/process" method="post">
-        <label for="cpf">CPF</label><br>
-        <input type="text" id="cpf" name="cpf">
-        <br>
-        <label for="password">Senha</label><br>
-        <input type="password" id="password" name="senha">
-        <br>
-        <button type="submit" class="button-5" name="botaoLogin">Entrar</button>
-    </form>
-    <p>Ainda não possui conta? <a href="/cadastro">Cadastre-se</a></p>
+    
+        if (isset($_SESSION['erros'])) {
+            foreach ($_SESSION['erros'] as $campo => $erro) {
+                echo '<p>' . $campo . ':' . $erro . '</p>';
+            }
+            unset($_SESSION['erros']);
+        }
+        ?>
+        <form action="login/process" method="post">
+            <label for="cpf">CPF</label><br>
+            <input type="text" id="cpf" name="cpf">
+            <br>
+            <label for="password">Senha</label><br>
+            <input type="password" id="password" name="senha">
+            <br>
+            <button type="submit" class="button-5" name="botaoLogin">Entrar</button>
+        </form>
+        <p>Ainda não possui conta? <a href="/cadastro">Cadastre-se</a></p>
+    </div>
 </div>
 </body>
 </html>
